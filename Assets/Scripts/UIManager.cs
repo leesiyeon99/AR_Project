9efbabb28627cf930b4e52872bf91bde9ea0bd87
leesiyeon99 <qsqs9090@naver.com>
2024-10-09 01:27:21 +0900

    feat : 가구 목록 리스트 UI 완성

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,19 +7,39 @@ public class UIManager : MonoBehaviour
     [SerializeField] Animator animator;
 
     [SerializeField] Button CloseListButton;
-
     [SerializeField] Button ListButton;
     private bool isListOpen = false;
 
     [SerializeField] Button chairListButton;
-    private bool isChairListOpen = false; 
+    [SerializeField] Button bedListButton;
+    [SerializeField] Button deskListButton;
+    [SerializeField] Button drawerListButton;
+    [SerializeField] Button lightListButton;
+    [SerializeField] Button mirrorListButton;
+    [SerializeField] Button sofaListButton;
+
+    [SerializeField] GameObject chairListObjects;
+    [SerializeField] GameObject bedListObjects;
+    [SerializeField] GameObject deskListObjects;
+    [SerializeField] GameObject drawerListObjects;
+    [SerializeField] GameObject lightListObjects;
+    [SerializeField] GameObject mirrorListObjects;
+    [SerializeField] GameObject sofaListObjects;
+
+    private GameObject curTarget = null;
 
     private void Start()
     {
         ListButton.onClick.AddListener(ToggleList);
         CloseListButton.onClick.AddListener(CloseList);
 
-        chairListButton.onClick.AddListener(ToggleChairList); 
+        chairListButton.onClick.AddListener(() => ToggleUIList(chairListObjects));
+        bedListButton.onClick.AddListener(() => ToggleUIList(bedListObjects));
+        deskListButton.onClick.AddListener(() => ToggleUIList(deskListObjects));
+        drawerListButton.onClick.AddListener(() => ToggleUIList(drawerListObjects));
+        lightListButton.onClick.AddListener(() => ToggleUIList(lightListObjects));
+        mirrorListButton.onClick.AddListener(() => ToggleUIList(mirrorListObjects));
+        sofaListButton.onClick.AddListener(() => ToggleUIList(sofaListObjects));
     }
 
     private void ToggleList()
@@ -40,19 +59,46 @@ public class UIManager : MonoBehaviour
     private void CloseList()
     {
         animator.Play("CloseList");
+        HideAllFurniture();
+        curTarget = null; 
     }
 
-    private void ToggleChairList()
+    private void ToggleUIList(GameObject targetList)
     {
-        isChairListOpen = !isChairListOpen; 
-
-        if (isChairListOpen)
+        if (curTarget == targetList)
         {
-            animator.Play("ChairOpenList");
+            StartCoroutine(CloseListRoutine(curTarget));
+            animator.Play("CloseUIList");
+            curTarget = null; 
         }
         else
         {
-            animator.Play("ChairCloseList"); 
+            if (curTarget != null)
+            {
+                animator.Play("OpenUIList"); 
+                curTarget.SetActive(false);
+            }
+
+            targetList.SetActive(true);
+            animator.Play("OpenUIList");
+            curTarget = targetList;
         }
+    }
+
+    private void HideAllFurniture()
+    {
+        chairListObjects.SetActive(false);
+        bedListObjects.SetActive(false);
+        deskListObjects.SetActive(false);
+        drawerListObjects.SetActive(false);
+        lightListObjects.SetActive(false);
+        mirrorListObjects.SetActive(false);
+        sofaListObjects.SetActive(false);
+    }
+
+    IEnumerator CloseListRoutine(GameObject targetList)
+    {
+        yield return new WaitForSeconds(0.2f);
+        targetList.SetActive(false);
     }
 }
